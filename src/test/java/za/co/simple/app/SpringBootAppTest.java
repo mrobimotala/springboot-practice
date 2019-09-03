d21e2a1a5f01059eaa1dc87209ba61715013108d
abrmagp <robert.motala@absa.africa>
2019-09-03 22:10:56 +0200

    added springboot starter

---
+++ src/test/java/za/co/simple/app/SpringBootAppTest.java
@@ -0,0 +1,15 @@
+package za.co.simple.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootApp.class)
+public class SpringBootAppTest {
+    @Test
+    public void contextLoads() {
+    }
+
+}
